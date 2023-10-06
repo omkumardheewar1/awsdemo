8005acd2f71f8991714f5a174bf665d204d66984
Om Dheewar <omkumardheewar@gmail.com>
2023-10-06 18:35:28 +0530

    initial commit

--- src/main/java/com/demo/awsdemo/HelloController.java
+++ src/main/java/com/demo/awsdemo/HelloController.java
@@ -9,6 +9,8 @@ public class HelloController {
     @GetMapping("/hello")
     public String greet() {
 
+
+
         //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
         return "Welcome to the AWS CI/CD Deployment Demo:::";
     }
